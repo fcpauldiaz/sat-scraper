b0f2e7e763a7117dc63070091ce6a97275d0390a
Pablo Díaz <pablo5_diaz@hotmail.com>
2021-08-07 17:19:01 -0600

    remove unused code

--- app.py
+++ app.py
@@ -112,29 +112,10 @@ def long_task(self):
             'result': results}
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'GET':
-        return render_template('index.html', email=session.get('email', ''))
-    email = request.form['email']
-    session['email'] = email
+    return render_template('index.html'))
 
-    # send the email
-    email_data = {
-        'subject': 'Hello from Flask',
-        'to': email,
-        'body': 'This is a test email sent from a background Celery task.'
-    }
-    if request.form['submit'] == 'Send':
-        # send right away
-        send_async_email.delay(email_data)
-        flash('Sending email to {0}'.format(email))
-    else:
-        # send in one minute
-        send_async_email.apply_async(args=[email_data], countdown=60)
-        flash('An email will be sent to {0} in one minute'.format(email))
-
-    return redirect(url_for('index'))
 
 
 @app.route('/longtask', methods=['POST'])
